5234e7b0e4399b2d1de8249bd53eeb72dfaf3430
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-24 21:36:37 +0100

    Creating feed route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Feeds from "../src/pages/Feeds"
 
 // Calling all components and using router to render pages
 function App() {
   return (
     <Router>
       <div>
+        <Route exact path="/feeds" component={Feeds} />
       </div>
     </Router>
   )
---
+++ client/src/pages/Feeds.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Feeds() {
+   
+    return (
+    <div>
+        <h1>feeds</h1>
+    </div>
+    )
+};
+
+
+export default Feeds;
