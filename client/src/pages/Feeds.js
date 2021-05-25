15ad0497209ce09dfb9c85f811a3a45d34c5decf
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-25 14:12:23 +0100

    Adding contibution form

--- client/src/pages/Feeds.js
+++ client/src/pages/Feeds.js
@@ -3,16 +3,16 @@ import React from "react";
 const styles = {
     cardStyle: {
         width: '60%',
-        padding :0
+        padding: 0
     },
     treeIcon: {
-        color : "green"
+        color: "green"
     },
     clockIcon: {
-        color : "red"
+        color: "red"
     },
     mapIcon: {
-        color : "brown"
+        color: "brown"
     }
 
 };
@@ -47,16 +47,45 @@ function Feeds() {
             <div className="row d-flex justify-content-center mb-3">
                 <div className="card" style={styles.cardStyle}>
                     <div className="card-header pb-0">
-                        <h5 className="card-title">Project Name <i className="fas fa-map-marked-alt ps-3" style={styles.mapIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Land needed"></i><i className="fas fa-clock ps-3" style={styles.clockIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Time needed"></i><i className="fas fa-tree ps-3" style={styles.treeIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Resources needed"></i></h5> 
+                        <h5 className="card-title">Project Name <i className="fas fa-map-marked-alt ps-3" style={styles.mapIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Land needed"></i><i className="fas fa-clock ps-3" style={styles.clockIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Time needed"></i><i className="fas fa-tree ps-3" style={styles.treeIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Resources needed"></i></h5>
                         <h6 className="text-muted">Username</h6>
 
                     </div>
                     <div className="card-body">
-                        <p className="card-text">"text"</p>
+                        <p className="card-text">Project description</p>
                     </div>
                     <div className="card-footer text-center">
                         <button className="btn btn-success btn-sm m-2" ><i className="fas fa-hands-helping"></i> Contribute</button>
                         <button className="btn btn-success btn-sm m-2"><i className="fas fa-envelope"></i> Contact me</button>
+                    </div>
+                </div>
+            </div>
+            {/* Contribute Card */}
+            <div className="row d-flex justify-content-center mb-3">
+                <div className="card" style={styles.cardStyle}>
+                    <div className="card-header bg-success">
+                        <h5 className="card-title text-white "><i className="fab fa-wpforms"></i> Contribution Form</h5>
+                    </div>
+                    <div className="card-body">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="Land" />
+                            <label class="form-check-label" for="Land">Land</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="Time" />
+                            <label class="form-check-label" for="Time">Time</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="Resources" />
+                            <label class="form-check-label" for="Resources">Resources</label>
+                        </div>
+                        <div class="input-group mt-3">
+                            <span class="input-group-text">Message</span>
+                            <input type="text" aria-label="First name" class="form-control" />
+                        </div>
+                    </div>
+                    <div className="card-footer text-center">
+                        <button className="btn btn-danger btn-sm m-2">Submit</button>
                     </div>
                 </div>
             </div>
