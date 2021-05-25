e2f494df5b80a005ca5e4c15f6a83a02dc380913
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-25 13:30:55 +0100

    Adding project card

--- client/src/pages/Feeds.js
+++ client/src/pages/Feeds.js
@@ -1,31 +1,65 @@
 import React from "react";
 
+const styles = {
+    cardStyle: {
+        width: '60%',
+        padding :0
+    },
+    treeIcon: {
+        color : "green"
+    },
+    clockIcon: {
+        color : "red"
+    },
+    mapIcon: {
+        color : "brown"
+    }
+
+};
 
 function Feeds() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
-                <div class="container-fluid">
+            {/* Navbar */}
+            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow mb-3">
+                <div className="container-fluid">
                     <img className='ps-3 pe-2' src='../../favicon-32x32.png' alt='icon'></img>
                     <a className="navbar-brand">TreePeeps</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Menu 
-                    <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Menu
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item ps-2"><a className="nav-link" href="#">Dashboard</a></li>
                             <li className="nav-item ps-2"><a className="nav-link" href="#">About us</a></li>
                             <li className="nav-item ps-2"><a className="nav-link" href="#">Contact us</a></li>
                             <li className="nav-item ps-2"><a className="nav-link" href="#">logout</a></li>
                         </ul>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
             </nav>
+            {/* Post Card */}
+            <div className="row d-flex justify-content-center mb-3">
+                <div className="card" style={styles.cardStyle}>
+                    <div className="card-header pb-0">
+                        <h5 className="card-title">Project Name <i className="fas fa-map-marked-alt ps-3" style={styles.mapIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Land needed"></i><i className="fas fa-clock ps-3" style={styles.clockIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Time needed"></i><i className="fas fa-tree ps-3" style={styles.treeIcon} data-bs-toggle="tooltip" data-bs-placement="top" title="Resources needed"></i></h5> 
+                        <h6 className="text-muted">Username</h6>
+
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text">"text"</p>
+                    </div>
+                    <div className="card-footer text-center">
+                        <button className="btn btn-success btn-sm m-2" ><i className="fas fa-hands-helping"></i> Contribute</button>
+                        <button className="btn btn-success btn-sm m-2"><i className="fas fa-envelope"></i> Contact me</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
