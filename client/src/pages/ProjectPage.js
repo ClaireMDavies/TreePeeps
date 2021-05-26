f8df604f401626d6df0f89dd04ef25493ae74187
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-26 15:19:58 +0100

    Set up the basic page

--- client/src/pages/ProjectPage.js
+++ client/src/pages/ProjectPage.js
@@ -3,11 +3,58 @@ import Navbar from "Navbar";
 
 
 const Project = () => {
-    return ( 
-     <div className="App">
-              <Navbar/>
-    </div>
-     );
+    return (
+        <main class="container-fluid">
+
+            <div class="row p-3 myDashboard">
+                <h1>New Project Form</h1>
+            </div>
+
+            <div class="row p-3">
+                <div class="col">
+                    <div class="row p-3 rounded m-2">
+                        <div class="dashCard" style="width: 20rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Land Specifications</h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Area(m2): </li>
+                                <li class="list-group-item">Location: </li>
+                                <li class="list-group-item">Owner: </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="row p-3 rounded m-2">
+                        <div class="dashCard" style="width: 20rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Time Specifications</h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Work Hours Needed: </li>
+                                <li class="list-group-item">Number of trees: </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="row p-3 rounded m-2">
+                        <div class="dashCard" style="width: 20rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Resource Specifications</h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Trees: </li>
+                                <li class="list-group-item">Spades: </li>
+                                <li class="list-group-item">Fertilizer: </li>
+                                <li class="list-group-item">Stakes: </li>
+                                <li class="list-group-item">Spirals: </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
- 
+
 export default Project;
