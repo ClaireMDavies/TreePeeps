b1a2997d95f4c8ba70dbaf47b19948922580c553
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-26 21:19:28 +0100

    Created routes in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import dashboard from "./pages/dashboard";
+
 
 // Calling all components and using router to render pages
 function App() {
   return (
     <Router>
       <div>
+        <Route exact path="/dashboard" component={dashboard} />
+      </div>
+      <div>
+        <Route exact path="/" component={dashboard} />
       </div>
     </Router>
   )
