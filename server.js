78a7d46075f3b16ecd543713789c74079d6bd545
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-26 20:14:33 +0100

    Creating a session with mongoDb

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const path = require("path");
 // const mongoose = require("mongoose");
 // const routes = require("./routes")
-const PORT = process.env.PORT || 3000;
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(session({
+  secret: 'SECRET KEY',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://localhost:27017/sessions',
+})
+}));
 
 // Define middleware 
 app.use(express.urlencoded({ extended: true }));
