121159e83c4eb5577a32dc60bf96002216a96e9f
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-26 19:11:03 +0100

    Added a default route, to get app working locally

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Project from "../src/pages/ProjectPage"
+import Project from "../src/pages/ProjectPage";
 
 // Calling all components and using router to render pages
 function App() {
@@ -8,6 +8,9 @@ function App() {
     <Router>
       <div>
         <Route exact path="/ProjectPage" component={Project} />
+      </div>
+      <div>
+        <Route exact path="/" component={Project} />
       </div>
     </Router>
   )
