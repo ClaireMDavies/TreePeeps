2a813bf02c6335e41344a2f413498c1cc2760cce
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-29 11:48:54 +0100

    changed signup to SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
+import SignUp from "../src/pages/SignUp";
 import Project from "../src/pages/ProjectPage";
 import Feeds from "../src/pages/Feeds";
 import Login from "../src/pages/Login";
-import ContactUs from "./pages/ContactUs";
-import Dashboard from "./pages/Dashboard";
+import ContactUs from "../src//pages/ContactUs";
+import Dashboard from "../src/pages/Dashboard";
 
 
 function App() {
