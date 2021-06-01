cbf5f911b27468d18aeb32d802bb30f1ddc6ba04
luke <lukedesond@hotmail.co.uk>
2021-06-01 20:51:05 +0100

    background fixed

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,22 +1,32 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Image1 from "../assets/TREEPEEPS.png";
+import "../styles/homepage.css"
+import logo from "../assets/TREEPEEPS.png"
+function HomePage() {
+    return (
+        <div>
+            <Navbar />
+            <header className="masthead">
+                <div className="container d-flex h-100 align-items-center">
+                    <div className="mx-auto text-center">
+                        <img id="logo" src={logo} /> 
+                        <h3 className="mx-auto mt-4">A tree-planting app for tree-minded people</h3>
+                        <p>Tiny Forests bring all the benefits of a forest – reconnecting people with nature and raising awareness, helping to mitigate the impacts of climate change, as well as providing nature-rich habitat patches to support urban wildlife – right into the heart of our cities and urban spaces.
+                        Earthwatch, 2021</p>
+                        <ul className="buttons">
+                            <li>
+                                <button className="btn btn-outline-success btn-lg m-2" >Login</button>
+                            </li>
+                            <li>
+                                <button className="btn btn-outline-success btn-lg m-2" >Sign Up</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+        </div>
 
-function HomePage(){
-return(
-    <div>
-        <Navbar />
-        <div style={{ 
-      backgroundImage: `url(${Image1})`
-    }}></div>
-    <div>
-        <h1>TreePeeps</h1>
-        <button>Login</button>
-        <button>Sign Up</button>
-    </div>
-    </div>
-
-)
+    )
 }
 
 export default HomePage;
