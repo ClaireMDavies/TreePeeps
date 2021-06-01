3bf192efb163cce82ce339fb182f9de9c0a5b65c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-01 21:27:46 +0100

    changed to Reactstrap

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { FormGroup, Input, Button } from 'reactstrap';
 
 const SearchForm = () => {
     return (
 
         <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <FormGroup>
+                <Input type="text" name="message" id="message" placeholder="search-by-location..." />
+            </FormGroup><br></br>  
+            <Button type="submit" value="submit">Search</Button>
         </form>
 
     );
