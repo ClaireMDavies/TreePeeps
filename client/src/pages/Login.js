78b4ef17625e6e8e8e0a2789c8dff90175e22b8d
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-02 20:58:44 +0100

    added more styling from RS

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Card, CardBody, Form, Input, Button } from 'reactstrap';
 import Navbar from "../components/NavbarTreePeeps";
 import loginImg from '../images/login-img.jpg';
 import NavItem from "../components/NavItem";
 
 const styles = {
     cardStyle: {
-        width: '60%',
+        width: '70%',
         padding: 0
     },
     cardSpan: {
@@ -40,37 +40,37 @@ function Login() {
                 </NavItem>
             </Navbar>
             {/* Login Card */}
-            <Container>
-                <div className="card border-success" style={styles.cardStyle}>
+            <Container className="d-flex justify-content-center mb-3 mt-5">
+                <Card className="border-success" style={styles.cardStyle}>
                     <Row>
                         <Col xs="6">
                             <img className="img-fluid" src={loginImg} alt="login" />
                         </Col>
                         <Col xs="6">
-                            <div className="card-body">
-                                <div className="brand-wrapper">
+                            <CardBody>
+                                <Container>
                                     <img className='pe-2 pb-2' src='../../favicon-32x32.png' alt='icon'></img>
                                     <span className="fs-4">TreePeeps</span>
-                                </div>
+                                </Container>
                                 <p className="login-card-description">Sign into your account</p>
-                                <form >
+                                <Form>
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" style={styles.cardSpan} >Email  </span>
-                                        <input type="email" className="form-control" placeholder="Email" aria-label="Email" />
+                                        <Input type="email" className="form-control" placeholder="Email" aria-label="Email" />
                                     </div>
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" style={styles.cardSpan} >Password </span>
-                                        <input type="password" className="form-control" placeholder="Password" aria-label="Password" />
+                                        <Input type="password" className="form-control" placeholder="Password" aria-label="Password" />
                                     </div>
-                                </form>
+                                </Form>
                                 <div className="text-center">
-                                    <button type="submit" className="btn btn-danger btn-sm m-2">Submit</button>
+                                    <Button type="submit" color="danger">Submit</Button>
                                 </div>
                                 <p className="login-card-footer-text">Don't have an account? <a href="#!" className="text-reset">Register here</a></p>
-                            </div>
+                            </CardBody>
                         </Col>
                     </Row>
-                </div>
+                </Card>
             </Container>
         </div>
     )
