c9e85949fa904a30f2d98ff839c90398fd176f1b
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-02 20:33:19 +0100

    undid card from RS

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import Navbar from "../components/NavbarTreePeeps";
 import NavItem from "../components/NavItem";
 import "../styles/dashboard.css";
