02298d5f69c722b9c0a40fc76ded54a40fd88e09
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-02 19:11:28 +0100

    changed from reactstrap to bootstrap style

--- client/src/components/NavbarTreePeeps/index.js
+++ client/src/components/NavbarTreePeeps/index.js
@@ -1,48 +1,27 @@
-import React, { useState } from "react";
-import { Collapse, Nav, Navbar, NavbarToggler, NavbarBrand, NavLink } from 'reactstrap';
-import NavItem from '../NavItem';
+import React from "react";
 import SearchForm from "../SearchForm";
 
-function NavbarTreePeeps() {
-    const [collapsed, setCollapsed] = useState(true);
-
-    const toggleNavbar = () => setCollapsed(!collapsed);
-
-    // return (
-    //     < Navbar className="navbar navbar-expand-lg navbar-light bg-light shadow mb-3" >
-    //         <Container>
-    //             <img className='ps-3 pe-2' src='../../favicon-32x32.png' alt='icon'></img>
-    //             <a className="navbar-brand">TreePeeps</a>
-    //             <NavbarBrand href="/" className="mr-auto">TreePeeps</NavbarBrand>
-    //             <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-    //             <Button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Menu
-    //                 <span className="navbar-toggler-icon"></span>
-    //             </Button>
-    //             <Collapse isOpen={!collapsed} navbar>
-    //                 <NavItem />
-
-    //                 {window.location.pathname === "/dashboard" || window.location.pathname === "/feeds" || window.location.pathname === "/project"
-    //                     ? <SearchForm />
-    //                     : ""
-    //                 }
-    //             </Collapse>
-    //         </Container>
-    //     </ Navbar >
-    // );
+const NavbarTreePeeps = (props) => {
     return (
-        <div>
-            <Navbar color="faded" light>
+        < nav className="navbar navbar-expand-lg navbar-light bg-light shadow mb-3" >
+            <div className="container-fluid">
                 <img className='ps-3 pe-2' src='../../favicon-32x32.png' alt='icon'></img>
-                <NavbarBrand href="/" className="mr-auto"><b>Tree</b>Peeps</NavbarBrand>
-                <SearchForm />
-                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-                <Collapse isOpen={!collapsed} navbar>
-                    <Nav navbar>
-                        <NavItem />
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
+                <a className="navbar-brand">TreePeeps</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Menu
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {props.children}
+
+                    </ul>
+                    {window.location.pathname === "/dashboard" || window.location.pathname === "/feeds" || window.location.pathname === "/project"
+                        ? <SearchForm />
+                        : ""
+                    }
+                </div>
+            </div>
+        </nav >
     );
 }
 
