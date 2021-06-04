ba28af521b816afc642a5aafdd057709c066e047
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-04 01:48:41 +0100

    created connection.js

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/currenseed', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+
+mongoose.connection.on('connected', () => console.log('Connected to MongoDB Endpoint'));
+mongoose.connection.on('error', (err) => console.log(`Mongoose default connection error: ${err}`));
+
+module.exports = mongoose.connection;
