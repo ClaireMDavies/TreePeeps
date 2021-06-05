bb16700fdc2ce736b3b3841efac6ee3f3faf0352
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-05 21:55:09 +0100

    amended for projectSchema

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
+const projectRoutes = require("./project");
 
 // user routes
 router.use("/user", userRoutes);
+
+//project routes
+
+router.use("/projects", projectRoutes);
 
 module.exports = router;
