84bfb5c7ed936d51eb2021fc1b5542a41fb870d8
Saadia El fekak <saadiaelfekak@gmail.com>
2021-06-10 00:47:50 +0100

    Fixing styling and responsiveness

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,48 +1,31 @@
 import React from "react";
 import { useState } from "react";
-import { Container, Row, Col, Card, CardBody, CardHeader, Form, Input, Button } from 'reactstrap';
+import { Container, Card, CardBody, CardHeader, Form, Input, Button } from 'reactstrap';
 import Navbar from "../components/NavbarTreePeeps";
 import loginImg from '../images/login-img.jpg';
 import NavItem from "../components/NavItem";
 import Footer from "../components/Footer";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
-import {toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
-
-
-const styles = {
-    cardStyle: {
-        width: '70%',
-        padding: 0
-    },
-    cardSpan: {
-        display: "inline - block",
-        width: "100px"
-    },
-
-
-};
 
 function Login(props) {
     const loginFailureNotification = () => toast("There was an error with your login please try again");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    
-    function signIn()
-    {
+
+    function signIn() {
         API.login(username, password)
-        .then(response => {
-            if (response.data.userId)
-            {
-                localStorage.setItem("userId", response.data.userId);
-                props.history.push("/dashboard");
-            }
-            else
-            {
-               loginFailureNotification();
-            }
-        });
+            .then(response => {
+                if (response.data.userId) {
+                    localStorage.setItem("userId", response.data.userId);
+                    props.history.push("/dashboard");
+                }
+                else {
+                    loginFailureNotification();
+                }
+            });
     }
 
     // const [userName, setUserName] = useState("");
@@ -103,33 +86,33 @@ function Login(props) {
             </Navbar>
             {/* Login Card */}
             <Container className="d-flex justify-content-center mb-3 mt-5">
-                <Card className="border-success p-0" >
-                    <Row>
-                        <Col xs="6" className='pe-0'>
+                <Card className="border-success" style={{width:'70%'}}>
+                    <div className="row no-gutters">
+                        <div className="col-md-5 pe-0">
                             <img className="img-fluid" src={loginImg} alt="login" />
-                        </Col>
-                        <Col xs="6" className='ps-0'>
-                        <CardHeader className="border-success" >  <img className='pe-2 pb-2' src='../../favicon-32x32.png' alt='icon'></img>
-                        <span className="fs-4">TreePeeps</span></CardHeader>
+                        </div>
+                        <div className="col-md-7 ps-0">
+                            <CardHeader className="border-success" >  <img className='pe-2 pb-2' src='../../favicon-32x32.png' alt='icon'></img>
+                                <span className="fs-4">TreePeeps</span></CardHeader>
                             <CardBody>
-                                  <h5 className="text-center mb-3">Sign into your account</h5>
+                                <h5 className="text-center mb-3">Sign into your account</h5>
                                 <Form>
                                     <div className="input-group mb-3">
-                                        <span className="input-group-text" style={styles.cardSpan} >Email  </span>
+                                        <span className="input-group-text" style={{ width: '100px' }}>Email  </span>
                                         <Input type="email" className="form-control" placeholder="Email" aria-label="Email" onChange={e => setUsername(e.target.value)} />
                                     </div>
                                     <div className="input-group mb-3">
-                                        <span className="input-group-text" style={styles.cardSpan} >Password </span>
+                                        <span className="input-group-text" style={{ width: '100px' }} >Password </span>
                                         <Input type="password" className="form-control" placeholder="Password" aria-label="Password" onChange={e => setPassword(e.target.value)} />
                                     </div>
                                 </Form>
                                 <div className="text-center">
-                                    <Button className="mt-3" type="submit" color="danger" onClick={signIn}>Login</Button>
+                                    <Button className="mt-3" type="submit" color="success" onClick={signIn}>Login</Button>
                                 </div>
                                 <p className="mt-3 text-center">Don't have an account? <Link className="text-reset" to="/signup">Register here</Link></p>
                             </CardBody>
-                        </Col>
-                    </Row>
+                        </div>
+                    </div>
                 </Card>
             </Container>
             <Footer />
