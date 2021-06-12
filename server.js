afecd309a1cf6119d00d376969d780cb1f7bad20
Claire Davies <claire.davies@junitech.com>
2021-06-12 20:55:53 +0100

    Resolving Heroku deployment issues

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 // Serve up static assets 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, './client/public')));
+  app.use(express.static('client/build'));
 }
 
 // Define API routes
