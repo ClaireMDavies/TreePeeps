74383050148c3e1e2a65ad78caf2a5295975ae13
Claire Davies <claire.davies@junitech.com>
2021-06-12 20:50:55 +0100

    Resolving Heroku deployment issues

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 // Serve up static assets 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, './client/public')));
 }
 
 // Define API routes
@@ -34,7 +34,7 @@ app.use(routes);
 
 // wildcard handler for all pages
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // Connect to the Mongo DB
