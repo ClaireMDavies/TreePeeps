96106bb57f82b394ec2d17df9b1a46e6e1e72d49
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-15 17:34:38 +0100

    added further records with new format

--- seeders/seed.js
+++ seeders/seed.js
@@ -16,22 +16,37 @@ db.once('open', async () => {
         password: 'Password12345',
         country: 'UK',
         city: 'Redditch',
-        latitude: -1.96054745742439,
-        longitude: 52.292611432931295,
+        latitude: 52.292611432931295,
+        longitude: -1.96054745742439,
         timestamps: new Date().setDate(new Date().getDate() - 9)
     });
 
     const sallyUser = await User.create({
-        username: 'SallyR',
+        username: 'sallyxp',
         firstname: 'Sally',
         lastname: 'Rodgers',
         email: 'sally@email.com',
         password: 'Password12345',
         country: 'UK',
         city: 'Hagley',
-        latitude: -2.134756486816054,
-        longitude: 52.43862848771243,
+        latitude: 52.43862848771243,
+        longitude: -2.134756486816054,
         timestamps: new Date().setDate(new Date().getDate() - 9)
+    });
+
+    const JackSilverUser = await User.create({
+        
+            username: 'SilverT',
+            firstname: 'John',
+            lastname: 'Silver',
+            email: 'silverjack@email.com',
+            password: 'Pass1234',
+            country: 'UK',
+            city: 'Redditch',
+            latitude: -1.96054745742439,
+            longitude: 52.292611432931295,
+            timestamps: new Date().setDate(new Date().getDate() - 9)
+        
     });
 
     console.log('Users seeded');
@@ -51,7 +66,7 @@ db.once('open', async () => {
             latitude: 52.42480412578006,
             longitude: -2.1172434423933897,
             area: 120,
-            landOwner: "Ragley Estates",
+            landOwner: "Hagley Estates",
             hoursNeeded: 80,
             numTrees: 40,
             contributions: [
@@ -153,7 +168,7 @@ db.once('open', async () => {
             latitude: 52.574877055486695,
             longitude: -1.8569798837185685,
             area: 240,
-            landOwner: "Birmingham City Council",
+            landOwner: "Sutton Council",
             hoursNeeded: 45,
             numTrees: 425,
             contributions: [],
@@ -161,17 +176,45 @@ db.once('open', async () => {
             amtFertilizer: 10,
             numSpirals: 20,
             status: true,
-            otherResources: "n/a",
+            otherResources: "hazel rods to hold stakes together",
             timestamps: new Date().setDate(new Date().getDate() - 9),
         }
     );
+
+    const MemoryGardenProject = await Project.create(
+        {
+            owner: sallyUser._id,
+            name: "Queens Park",
+            title: "Help us build a Memory Garden",
+            description: "We should like to build a memory garden to commemorate those who have lost their lives during the pandemic, regardless of their cause of death.",
+            startDate: new Date(),
+            endDate: new Date().setDate(new Date().getDate() + 120),
+            location: {
+                type: "Point",
+                coordinates: [-1.8569798837185685, 52.574877055486695]
+            },
+            latitude: 52.574877055486695,
+            longitude: -1.8569798837185685,
+            area: 240,
+            landOwner: "Birmingham City Council",
+            hoursNeeded: 30,
+            numTrees: 12,
+            contributions: [],
+            numStakes: 50,
+            amtFertilizer: 20,
+            numSpirals: 25,
+            status: true,
+            otherResources: "hazel rods to hold stakes together & benches if you have them, old or new.",
+            timestamps: new Date().setDate(new Date().getDate() - 9),
+        }
+    )
 
     const heartOfEnglandForestProject = await Project.create(
         {
             owner: claireUser._id,
             name: "Heart of England Forest",
             title: "Help us expand this forest",
-            description: "The forest park has 600 acres available.",
+            description: "The forest park has over 500 acres.  We need people to help clear scrub, move saplings to better locations, help with maintenance.  ",
             startDate: new Date(),
             endDate: new Date().setDate(new Date().getDate() + 120),
             location: {
