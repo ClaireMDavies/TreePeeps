5f4c7aa41bae4c93b6c8adbb48264780a778a21a
Saadia El fekak <saadiaelfekak@gmail.com>
2021-06-16 20:46:52 +0100

    fixing path issue

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// // Serve up static assets 
+// Serve up static assets 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"/client/build")));
+  app.use(express.static("client/build"));
 }
 
 // Connect to the Mongo DB
