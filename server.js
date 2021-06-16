fffda6d1f1c00150cfc03e759b07d30ed8db910f
Saadia El fekak <saadiaelfekak@gmail.com>
2021-06-16 20:09:26 +0100

    fixing path issue

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes")
+const routes = require("./routes");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.json());
 
 // // Serve up static assets 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname,"/client/build")));
 }
 
 // Connect to the Mongo DB
